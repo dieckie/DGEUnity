e34f57289442cfdf05095be0a4d4872d0802d3b7
dieckie <dieckieus@gmail.com>
2016-07-12 16:08:18 +0200

    Fixed Killborders

--- Assets/Scripts/AcornController.cs
+++ Assets/Scripts/AcornController.cs
@@ -6,16 +6,19 @@ public class AcornController : MonoBehaviour {
 	public float speed;
 
 	private Rigidbody2D rb;
-	void Start () {
+
+	void Start() {
 		rb = GetComponent<Rigidbody2D>();
-		rb.velocity = new Vector2(0f, - speed);
+		rb.velocity = new Vector2(0f, -speed);
 	}
 
-	void Update () {
+	void Update() {
 		
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		Destroy(col.gameObject);
+		if (col.CompareTag("Enemy")) {
+			Destroy(col.gameObject);
+		}
 	}
 }
