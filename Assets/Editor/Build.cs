142feb621577c469a87ccc71d53e1802309963f1
dieckie <dieckieus@gmail.com>
2016-07-28 21:26:25 +0200

    Build angepasst

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -2,17 +2,29 @@
 using UnityEditor;
 using System.Diagnostics;
 using System.IO;
+using System;
 
 public class Build {
 	[MenuItem("Build/Build")]
-	public static void BuildGame ()
-	{
-		string[] levels = new string[] {"Assets/Scene/scene.unity"};
-		FileUtil.DeleteFileOrDirectory("E:/Programmieren/Projekte/DGEUnity/AutoBuild/Android/");
-		Directory.CreateDirectory("E:/Programmieren/Projekte/DGEUnity/AutoBuild/Android/");
-		FileUtil.DeleteFileOrDirectory("E:/Programmieren/Projekte/DGEUnity/AutoBuild/Win64/");
-		Directory.CreateDirectory("E:/Programmieren/Projekte/DGEUnity/AutoBuild/Win64/");
-		BuildPipeline.BuildPlayer(levels, "E:/Programmieren/Projekte/DGEUnity/AutoBuild/Android/DGE_android.apk", BuildTarget.Android, BuildOptions.None);
-		BuildPipeline.BuildPlayer(levels, "E:/Programmieren/Projekte/DGEUnity/AutoBuild/Win64/DGE_win64.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+	public static void BuildGame() {
+		string[] args = Environment.GetCommandLineArgs();
+		string path = "";
+		for(int i = 0; i < args.Length; i++) {
+			if(args[i].Equals("Build.BuildGame")) {
+				path = args[i + 1];
+				break;
+			}
+		}
+		if(!path.EndsWith("/")) {
+			path += "/";
+		}
+		UnityEngine.Debug.Log("PATH: "  + path);
+		string[] levels = new string[] { "Assets/Scene/scene.unity" };
+		FileUtil.DeleteFileOrDirectory(path + "Android/");
+		Directory.CreateDirectory(path + "Android/");
+		FileUtil.DeleteFileOrDirectory(path + "Win64/");
+		Directory.CreateDirectory(path + "Win64/");
+		BuildPipeline.BuildPlayer(levels, path + "Android/DGE_android.apk", BuildTarget.Android, BuildOptions.None);
+		BuildPipeline.BuildPlayer(levels, path + "Win64/DGE_win64.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
 	}
 }
